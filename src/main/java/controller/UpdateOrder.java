dce8e53dbac44ca5dd81a9a2d6f22de2b0d7a6bf
NguyenTanDat <nguyendat11577@gmail.com>
2023-12-01 15:14:08 +0700

    check order

--- src/main/java/controller/UpdateOrder.java
+++ src/main/java/controller/UpdateOrder.java
@@ -18,16 +18,29 @@ import java.util.List;
 public class UpdateOrder extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
         String action = request.getParameter("action");
         String id = request.getParameter("id");
-        if(action!=null){
-            if(action.equals("delete")){
+        if (action != null) {
+            if (action.equals("delete")) {
                 OrderDetailService.deleteOrderDetail(id);
                 OrderService.deleteOrder(id);
                 response.sendRedirect("ListOrder");
             }
-            if(action.equals("update")){
+            if (action.equals("update")) {
                 OrderDetailService.updateStatus(id);
+                response.sendRedirect("ListOrder");
+            }
+            if (action.equals("cancel")) {
+                OrderDetailService.cancelOrder(id);
+                UserSession u = (UserSession) request.getSession().getAttribute("user");
+                String subject = "Thông báo đặt hàng";
+                String content = "Cảm ơn bạn đã đặt hàng \n" +
+                        "Tôi rất tiếc khi phải thông báo rằng đơn hàng của bạn đã bị người khác chỉnh sửa thông tin, bạn có thể đặt hàng lại nhé! \n" +
+                        "cảm ơn bạn đã tin dùng sản phẩm của chúng tôi";
+                SendToMail.sendEmail(u.getEmail(), subject, content);
                 response.sendRedirect("ListOrder");
             }
         }
